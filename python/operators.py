f957704430c2f495275f60ff4515438e26088397
Anna Elizabeth Woodard <awoodard@nd.edu>
2017-10-23 19:28:15 -0400

    Cleanup unused files

--- python/operators.py
+++
@@ -1,4 +0,0 @@
-# operators = ['c2W', 'c3G', 'c3W', 'cA', 'cB', 'cG', 'cHB', 'cHQ', 'cHW',
-#              'cHd', 'cHu', 'cHud', 'cT', 'cWW', 'cpHQ', 'cu', 'cuB',
-#              'cuG', 'cuW', 'tc3G', 'tc3W', 'tcG', 'tcHW']
-operators = ['c2B', 'c2G', 'c2W', 'c3G', 'c3W', 'c6', 'cA', 'cB', 'cG', 'cH', 'cHB', 'cHL', 'cHQ', 'cHW', 'cHd', 'cHe', 'cHu', 'cHud', 'cT', 'cWW', 'cd', 'cdB', 'cdG', 'cdW', 'cl', 'clB', 'clW', 'cpHL', 'cpHQ', 'cu', 'cuB', 'cuG', 'cuW', 'tc3G', 'tc3W', 'tcA', 'tcG', 'tcHB', 'tcHW']
